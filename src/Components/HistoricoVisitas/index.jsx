f2b5fb1fc9a6be74a653a8e3b2b3990ca5d56d11
Andrielle Duarte <andrinduarte@gmail.com>
2025-08-07 23:35:28 -0300

    exibições de lista de historico com o tempo correto

--- src/Components/HistoricoVisitas/index.jsx
+++ src/Components/HistoricoVisitas/index.jsx
@@ -1,19 +1,19 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./style.css"
+import "./style.css";
 
 export default function HistoricoVisitas() {
   let params = useParams();
-  let visitante = params.id
-  
-  
+  let visitante = params.id;
+
   const [visitas, setVisitas] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:8000/visitas/historico/${visitante}`)
-      .then(res => setVisitas(res.data))
-      .catch(err => {
+    axios
+      .get(`http://localhost:8000/visitas/historico/${visitante}`)
+      .then((res) => setVisitas(res.data))
+      .catch((err) => {
         console.error("Erro ao buscar visitas:", err);
         alert("Erro ao buscar histórico");
       });
@@ -21,34 +21,35 @@ export default function HistoricoVisitas() {
 
   return (
     <div className="containerVisitas">
-                <h2>Historico de visitas - Visitante {visitante}</h2>
-                {visitas.length > 0 ? (
-                    <table className="tabelaVisitas">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Motivo da Visita</th>
-                                <th>Data de entrada</th>
-                                <th>Data de saída</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {visitas.map((v, index) => (
-                                <tr key={index}>
-                                    <td>{v.id}</td>
-                                    <td>{v.motivo_visita}</td>
-                                    <td>{v.data_entrada}</td>
-                                    <td>{v.data_saida ? new Date(v.data_saida).toLocaleString() : ""}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                ) : (
-                    <p>Nenhum visitante encontrado.</p>
-                )}
-
-
-            </div>
+      <h2>Historico de visitas - Visitante {visitante}</h2>
+      {visitas.length > 0 ? (
+        <table className="tabelaVisitas">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Motivo da Visita</th>
+              <th>Data de entrada</th>
+              <th>Data de saída</th>
+            </tr>
+          </thead>
+          <tbody>
+            {visitas.map((v, index) => (
+              <tr key={index}>
+                <td>{v.id}</td>
+                <td>{v.motivo_visita}</td>
+                <td>{new Date(v.data_entrada + "Z").toLocaleString()}</td>
+                <td>
+                  {v.data_saida
+                    ? new Date(v.data_saida + "Z").toLocaleString()
+                    : ""}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Nenhum visitante encontrado.</p>
+      )}
+    </div>
   );
 }
-
