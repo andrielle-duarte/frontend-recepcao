fc6baaaaa041bf2c3a350e99f483066af604f416
Andrielle Nascimento Duarte <andrielle.duarte@intranet.ifrj.edu.br>
2025-09-12 13:36:14 -0300

    keycloak rodando localmente

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -1,22 +1,23 @@
-// LoginButton.jsx
-import React from 'react';
 import keycloak from '../../keycloak';
 import Topo from '../Topo';
+import './style.css';
 
 function LoginButton() {
   const handleLogin = () => {
-    keycloak.login(); // Vai redirecionar para o login do Keycloak
+    keycloak.login();
   };
 
   return (
     <>
       <Topo />
-      <div className="formLogin">
-        <div className="login">
-          <h2 className='titulo' >Entrar no sistema</h2>
-          <button className='btnVisitantes' onClick={handleLogin}>
-          Usar Credenciais
-        </button>
+      <div className="containerLogin">
+        <div className="formLogin">
+          <h2 className='titulo' >Entrar no sistema de Recepção</h2>
+          <div className="login">
+            <button className='btnVisitantes' onClick={handleLogin}>
+              Usar Credenciais
+            </button>
+          </div>
         </div>
       </div>
     </>
